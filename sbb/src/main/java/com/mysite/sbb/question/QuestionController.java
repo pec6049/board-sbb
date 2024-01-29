b9c04def6afae05a1439e8ebc4e4427c0f2f5f27
pec6049 <pec6049@naver.com>
2024-01-29 20:29:52 +0900

    Add create

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,5 +31,16 @@ public class QuestionController {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
+	}
+	
+	@GetMapping(value = "/create")
+	public String questionCreate() {
+		return "question_form";
+	}
+	
+	@PostMapping(value = "/create")
+	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+		this.questionService.create(subject, content); //질문을 저장한다.
+		return "redirect:/question/list"; //질문 저장 후 질문 목록으로 이동
 	}
 }
