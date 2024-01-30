f38631729b0429da6dc65a37d20de93eedccc859
pec6049 <pec6049@naver.com>
2024-01-30 20:45:08 +0900

    Add Error Message

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -34,13 +35,16 @@ public class QuestionController {
 	}
 	
 	@GetMapping(value = "/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
 	@PostMapping(value = "/create")
-	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-		this.questionService.create(subject, content); //질문을 저장한다.
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent()); //질문을 저장한다.
 		return "redirect:/question/list"; //질문 저장 후 질문 목록으로 이동
 	}
 }
